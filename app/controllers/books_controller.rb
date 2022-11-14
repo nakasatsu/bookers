e62952bb9f41a8c6a532bb8961ab81353d50fbd3
EC2 Default User <ec2-user@ip-172-31-16-95.ap-northeast-1.compute.internal>
2022-11-14 11:41:53 +0000

    [Add]投稿・削除処理成功後のフラッシュメッセージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,6 +6,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
+      flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
       render :new
@@ -25,11 +26,15 @@ class BooksController < ApplicationController
   end
 
   def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
   end
 
   def destroy
     book = Book.find(params[:id])
     book.destroy
+    flash[:notice] = "successfully"
     redirect_to books_path
   end
 
