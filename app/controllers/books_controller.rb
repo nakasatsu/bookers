c736e02544ebf674a0dca50fcbc618f2d9a46c8b
EC2 Default User <ec2-user@ip-172-31-16-95.ap-northeast-1.compute.internal>
2022-11-15 09:14:58 +0000

    編集処理後のサクセスメッセージ追加,一覧画面に投稿フォーム表示

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,7 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
+  # def new
+    # @book = Book.new
+  # end
 
   def create
     @book = Book.new(book_params)
@@ -9,12 +9,14 @@ class BooksController < ApplicationController
       flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
-      render :new
+      @books = Book.all
+      render :index
     end
   end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -27,14 +29,19 @@ class BooksController < ApplicationController
 
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    if book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(book.id)
+    else
+      @book = Book.find(params[:id])
+      render :edit
+    end
   end
 
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    flash[:notice] = "successfully"
+    flash[:notice] = "Book was successfully destroyed."
     redirect_to books_path
   end
 
